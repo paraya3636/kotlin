446a8589753c05ace408496d0c8190b17e9ae4d9
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-11-08 11:18:48 +0100

    Move JvmRuntimeDescriptorLoaderTest to core/descriptors.runtime/tests
    This allows to get rid of the dependency on descriptors.runtime from
    compiler/IDE tests which is problematic: classes there clash with the
    classes in kotlin-reflect, which reference declarations in shadowed
    packages

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     testCompile(projectTests(":generators:test-generator"))
     testCompile(project(":compiler:ir.ir2cfg"))
     testCompile(project(":compiler:ir.tree")) // used for deepCopyWithSymbols call that is removed by proguard from the compiler TODO: make it more straightforward
+    testCompile(project(":core:descriptors.runtime"))
     testCompileOnly(project(":kotlin-daemon-client"))
     otherCompilerModules.forEach {
         testCompileOnly(project(it))
--- core/descriptors.runtime/build.gradle.kts
+++ core/descriptors.runtime/build.gradle.kts
@@ -1,20 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 apply { plugin("kotlin") }
 
-jvmTarget = "1.6"
-javaHome = rootProject.extra["JDK_16"] as String
-
 dependencies {
-    compile(project(":core:util.runtime"))
-    compile(project(":core:descriptors"))
-    compile(project(":core:descriptors.jvm"))
+    compileOnly(project(":core:util.runtime"))
+    compileOnly(project(":core:descriptors"))
+    compileOnly(project(":core:descriptors.jvm"))
+
+    testCompile(projectTests(":compiler:tests-common"))
+    testCompile(projectTests(":generators:test-generator"))
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" {}
+    "test" { projectDefault() }
 }
 
 tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.6"
-    targetCompatibility = "1.6"
+    if (name == "compileJava") {
+        sourceCompatibility = "1.6"
+        targetCompatibility = "1.6"
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    if (name == "compileKotlin") {
+        kotlinOptions.jvmTarget = "1.6"
+        kotlinOptions.jdkHome = rootProject.extra["JDK_16"] as String
+    }
+}
+
+val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateRuntimeDescriptorTestsKt")
+
+projectTest {
+    workingDir = rootDir
 }
